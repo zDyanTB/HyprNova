520b06073549fddf7e3dd4dabc727a5e1f0c1668
zDyant <zdyant@protonmail.com>
2024-02-20 12:27:34 -0300

    Add script to change wallpaper alongside rofi-colors

---
+++ .config/hypr/scripts/wallpaperSelect.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# This script for selecting wallpapers (SUPER W)
+
+SCRIPTSDIR="$HOME/.config/hypr/scripts"
+
+
+# WALLPAPERS PATH
+wallDIR="$HOME/Pictures/wallpapers"
+
+# Transition config
+FPS=60
+TYPE="wipe"
+DURATION=2
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION"
+
+# Check if swaybg is running
+if pidof swaybg > /dev/null; then
+  pkill swaybg
+fi
+
+# Retrieve image files
+PICS=($(ls "${wallDIR}" | grep -E ".jpg$|.jpeg$|.png$|.gif$"))
+RANDOM_PIC="${PICS[$((RANDOM % ${#PICS[@]}))]}"
+RANDOM_PIC_NAME="${#PICS[@]}. random"
+
+# Get the colors from pywal
+$SCRIPTSDIR/pickRofiColors.sh
+
+# Rofi command
+rofi_command="rofi -show -dmenu -theme $HOME/.config/rofi/themes/wallpaperSelector.rasi"
+
+menu() {
+  for i in "${!PICS[@]}"; do
+    # Displaying .gif to indicate animated images
+    if [[ -z $(echo "${PICS[$i]}" | grep .gif$) ]]; then
+      printf "$(echo "${PICS[$i]}" | cut -d. -f1)\x00icon\x1f${wallDIR}/${PICS[$i]}\n"
+    else
+      printf "${PICS[$i]}\n"
+    fi
+  done
+
+  printf "$RANDOM_PIC_NAME\n"
+}
+
+swww query || swww init
+
+main() {
+  choice=$(menu | ${rofi_command})
+
+  # No choice case
+  if [[ -z $choice ]]; then
+    exit 0
+  fi
+
+  # Random choice case
+  if [ "$choice" = "$RANDOM_PIC_NAME" ]; then
+    swww img "${wallDIR}/${RANDOM_PIC}" $SWWW_PARAMS
+    exit 0
+  fi
+
+  # Find the index of the selected file
+  pic_index=-1
+  for i in "${!PICS[@]}"; do
+    filename=$(basename "${PICS[$i]}")
+    if [[ "$filename" == "$choice"* ]]; then
+      pic_index=$i
+      break
+    fi
+  done
+
+  if [[ $pic_index -ne -1 ]]; then
+    swww img "${wallDIR}/${PICS[$pic_index]}" $SWWW_PARAMS
+  else
+    echo "Image not found."
+    exit 1
+  fi
+}
+
+# Check if rofi is already running
+if pidof rofi > /dev/null; then
+  pkill rofi
+  exit 0
+fi
+
+main
+
+sleep 0.5
+${SCRIPTSDIR}/PywalSwww.sh
+sleep 0.2
+${SCRIPTSDIR}/Refresh.sh
