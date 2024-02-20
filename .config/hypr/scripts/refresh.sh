edbc2de6177d0a3bb8cd6dd12a195edd4c394d49
zDyant <zdyant@protonmail.com>
2024-02-20 15:33:43 -0300

    Added refresh script

---
+++ .config/hypr/scripts/refresh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Scripts for refreshing waybar, rofi, swaync, pywal colors
+
+scriptsDir=$HOME/.config/hypr/scripts
+UserScripts=$HOME/.config/hypr/UserScripts
+
+# Define file_exists function
+file_exists() {
+    if [ -e "$1" ]; then
+        return 0  # File exists
+    else
+        return 1  # File does not exist
+    fi
+}
+
+# Kill already running processes
+_ps=(waybar rofi swaync)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+sleep 0.2
+# Relaunch waybar
+waybar &
+
+# Update spicetify colors
+$scriptsDir/refreshSpiceify.sh
+
+# relaunch swaync
+sleep 0.5
+swaync > /dev/null 2>&1 &
+
+# Relaunching rainbow borders if the script exists
+sleep 1
+if file_exists "${UserScripts}/RainbowBorders.sh"; then
+    ${UserScripts}/RainbowBorders.sh &
+fi
+
+
+# for cava-pywal (note, need to manually restart cava once wallpaper changes)
+ln -sf "$HOME/.cache/wal/cava-colors" "$HOME/.config/cava/config" || true
+
+exit 0
