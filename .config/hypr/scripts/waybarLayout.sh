b01f2250be91b680fd4b97706af90966aaaa6fe4
zDyant <zdyant@protonmail.com>
2024-02-25 10:17:15 -0300

    Change directories on scripts

---
+++ .config/hypr/scripts/waybarLayout.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Thanks https://github.com/JaKooLit ! #
+# Script for waybar layout or configs
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Define directories
+waybar_themes="$HOME/.config/waybar/themes"
+waybar_config="$HOME/.config/waybar/config"
+scriptsDir="$HOME/.config/hypr/scripts"
+rofi_config="$HOME/.config/rofi/themes/waybar-layout.rasi"
+
+# Function to display menu options
+menu() {
+    options=()
+    while IFS= read -r file; do
+        options+=("$(basename "$file")")
+    done < <(find "$waybar_themes" -maxdepth 1 -type f -exec basename {} \; | sort)
+
+    printf '%s\n' "${options[@]}"
+}
+
+# Apply selected configuration
+apply_config() {
+    ln -sf "$waybar_themes/$1" "$waybar_config"
+    restart_waybar_if_needed
+}
+
+# Restart Waybar
+restart_waybar_if_needed() {
+    if pgrep -x "waybar" >/dev/null; then
+        pkill waybar
+        sleep 0.1  # Delay for Waybar to completely terminate
+    fi
+    "${scriptsDir}/refresh.sh" &
+}
+
+# Main function
+main() {
+    choice=$(menu | rofi -dmenu -config "$rofi_config")
+
+    if [[ -z "$choice" ]]; then
+        echo "No option selected. Exiting."
+        exit 0
+    fi
+
+    case $choice in
+        "no panel")
+            pgrep -x "waybar" && pkill waybar || true
+            ;;
+        *)
+            apply_config "$choice"
+            ;;
+    esac
+}
+
+# Kill Rofi if already running before execution
+if pgrep -x "rofi" >/dev/null; then
+    pkill rofi
+    exit 0
+fi
+
+main
---
+++ .config/hypr/scripts/waybarStyles.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Thanks https://github.com/JaKooLit ! #
+# Script for waybar styles
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Define directories
+waybar_colors="$HOME/.config/waybar/colors"
+waybar_style="$HOME/.config/waybar/style.css"
+scriptsDir="$HOME/.config/hypr/scripts"
+rofi_config="$HOME/.config/rofi/themes/waybar-style.rasi"
+
+# Function to display menu options
+menu() {
+    options=()
+    while IFS= read -r file; do
+        if [ -f "$waybar_colors/$file" ]; then
+            options+=("$(basename "$file" .css)")
+        fi
+    done < <(find "$waybar_colors" -maxdepth 1 -type f -name '*.css' -exec basename {} \; | sort)
+    
+    printf '%s\n' "${options[@]}"
+}
+
+# Apply selected style
+apply_style() {
+    ln -sf "$waybar_colors/$1.css" "$waybar_style"
+    restart_waybar_if_needed
+}
+
+# Restart Waybar if it's running
+restart_waybar_if_needed() {
+    if pgrep -x "waybar" >/dev/null; then
+        pkill waybar
+        sleep 0.1  # Delay for Waybar to completely terminate
+    fi
+    "${scriptsDir}/refresh.sh" &
+}
+
+# Main function
+main() {
+    choice=$(menu | rofi -dmenu -config "$rofi_config")
+
+    if [[ -z "$choice" ]]; then
+        echo "No option selected. Exiting."
+        exit 0
+    fi
+
+    apply_style "$choice"
+}
+
+# Kill Rofi if already running before execution
+if pgrep -x "rofi" >/dev/null; then
+    pkill rofi
+    exit 0
+fi
+
+main
