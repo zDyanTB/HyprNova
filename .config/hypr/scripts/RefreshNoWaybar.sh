534cb2c801e7f1ca66c1bff40a411b5cf29d9a27
zDyant <zDyant@protonmail.com>
2024-03-24 10:14:27 -0300

    Add new scripts

---
+++ .config/hypr/scripts/RefreshNoWaybar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+
+# Modified version of Refresh but no waybar refresh
+# Used by automatic wallpaper change
+# Modified inorder to refresh rofi background, Pywal, SwayNC
+
+SCRIPTSDIR=$HOME/.config/hypr/scripts
+UserScripts=$HOME/.config/hypr/UserScripts
+
+# Define file_exists function
+file_exists() {
+    if [ -e "$1" ]; then
+        return 0  # File exists
+    else
+        return 1  # File does not exist
+    fi
+}
+
+# Kill already running processes
+_ps=(rofi)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+# Pywal refresh
+${SCRIPTSDIR}/PywalSwww.sh &
+
+# Relaunching rainbow borders if the script exists
+sleep 1
+if file_exists "${UserScripts}/RainbowBorders.sh"; then
+    ${UserScripts}/RainbowBorders.sh &
+fi
+
+# for cava-pywal (note, need to manually restart cava once wallpaper changes)
+ln -sf "$HOME/.cache/wal/cava-colors" "$HOME/.config/cava/config" || true
+
+exit 0
---
+++ .config/hypr/scripts/TouchPad.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# For disabling touchpad.
+
+Touchpad_Device="asue1209:00-04f3:319f-touchpad"
+notif="$HOME/.config/swaync/images/bell.png"
+
+XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$(id -u)}
+STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+toggle_touchpad() {
+  if [ ! -f "$STATUS_FILE" ] || [ "$(cat "$STATUS_FILE")" = "false" ]; then
+    echo "true" > "$STATUS_FILE"
+    action="enabled"
+  else
+    echo "false" > "$STATUS_FILE"
+    action="disabled"
+  fi
+
+  notify-send -u low -i "$notif" "Touchpad $action"
+  hyprctl keyword "device:$Touchpad_Device:enabled" "$(cat "$STATUS_FILE")"
+}
+
+toggle_touchpad
